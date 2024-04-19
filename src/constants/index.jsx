774b30f98be88bd5f716ae34b720ca56f1de9afd
userabhishek19 <121710381+userabhishek19@users.noreply.github.com>
2024-04-20 00:49:50 +0530

    Initial commit

---
+++ src/constants/index.jsx
@@ -0,0 +1,51 @@
+import { BotMessageSquare } from "lucide-react";
+import { BatteryCharging } from "lucide-react";
+import { Fingerprint } from "lucide-react";
+import { ShieldHalf } from "lucide-react";
+import { PlugZap } from "lucide-react";
+import { GlobeLock } from "lucide-react";
+
+
+export const navItems = [
+  { label: "HOME", href: "#" },
+  { label: "SHOP", href: "#" },
+  { label: "CONTACT US", href: "#" },
+  { label: "STORIES", href: "#" },
+];
+
+
+export const STORIES = [
+  {
+    title: "Code merge made easy",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "Review code without worry",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "AI Assistance to reduce time",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "Share work in minutes",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+];
+
+export const SHOP = [
+  {
+    title: "Free",
+    price: "$0",
+    features: [
+      "Private board sharing",
+      "5 Gb Storage",
+      "Web Analytics",
+      "Private Mode",
+    ] 
+  }
+];
