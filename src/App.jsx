971b91eff22fd369a1e54811fdf6233d4bed512c
userabhishek19 <121710381+userabhishek19@users.noreply.github.com>
2024-04-20 11:52:20 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroPage from './pages/Hero'
+import Footer from './components/Footer'
+
+
 
 const App = () => {
   return (
     <div>
  <Navbar/>
  <HeroPage/>
+ <Footer />
+
+
     </div>
   )
 }
